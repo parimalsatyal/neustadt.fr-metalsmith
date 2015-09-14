ab554d53be1b24ed7b49fc5b618fc48c11f9b4f3
Parimal Satyal <parimal@realityequation.net>
2015-09-15 00:43:07 +0200

    set up basic Metalsmith workflow

--- build.js
+++
@@ -1,6 +0,0 @@
-var Metalsmith = require('metalsmith');
-
-Metalsmith(__dirname)
-  .source('./src')
-  .destination('./build')
-  .build()
---
+++ src/index.md
@@ -0,0 +1,7 @@
+---
+title: Home
+template: index.hbt
+---
+This is the index page.
+
+## title
